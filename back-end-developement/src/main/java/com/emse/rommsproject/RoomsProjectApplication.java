841e7a83254d19dd2bb91c59b1fcabf529811be2
Anass-ELGHAOUI <elghaoui.anass@gmail.com>
2019-12-17 12:52:24 +0100

    adding back end part

---
+++ back-end-developement/src/main/java/com/emse/rommsproject/RoomsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.emse.rommsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomsProjectApplication.class, args);
+    }
+
+}
