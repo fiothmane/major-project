56eb2716e2dcfb53a485afa51730654312329fd0
walid.ouchtiti <walid.ouchtiti@etu.emse.fr>
2019-12-23 17:08:41 +0100

    add ringer dto

---
+++ src/main/java/com/emse/spring/faircorp/DTO/RingerDto.java
@@ -0,0 +1,57 @@
+package com.emse.spring.faircorp.DTO;
+
+import com.emse.spring.faircorp.model.Light;
+import com.emse.spring.faircorp.model.Status;
+
+public class RingerDto {
+    private Long id;
+
+    private Integer level;
+
+    private Status status;
+
+    private Long roomId;
+
+    public RingerDto() {
+
+    }
+
+    public RingerDto(Light light) {
+        this.id = light.getId();
+        this.level = light.getLevel();
+        this.status = light.getStatus();
+        this.roomId = light.getRoom().getId();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+}
