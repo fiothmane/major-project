bf8b3036aedba0e85d4375e8dacfea483272139a
walid.ouchtiti <walid.ouchtiti@etu.emse.fr>
2020-01-06 16:47:02 +0100

    add cors allow origins

--- src/main/java/com/emse/spring/faircorp/controller/LightController.java
+++ src/main/java/com/emse/spring/faircorp/controller/LightController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/lights")
 @Transactional
 public class LightController {
